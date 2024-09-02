081a004ae143846b14fe6b5ac3d85508b80477cd
tadeowoto <tadeo.woto@gmail.com>
2024-09-02 17:41:58 -0300

    pug settings added

--- app.mjs
+++ app.mjs
@@ -10,6 +10,8 @@ const app = express();
 const port = 3000;
 
 app.use(express.static(path.join(__dirName), 'public'));
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine','pug'); //le indicamos a express que motor de renderizado vamnos a usar
 
 app.get('/gallery', gallery); //que es esto
 app.use(error.error404);
