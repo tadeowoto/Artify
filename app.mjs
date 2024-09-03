eb22f74663fac2d64f0b5097f08182d873424af8
tadeowoto <tadeo.woto@gmail.com>
2024-09-03 14:36:32 -0300

    app.mjs fixed

--- app.mjs
+++ app.mjs
@@ -1,25 +1,21 @@
 import express from 'express';
 import  path  from 'node:path';
-import { dirname } from 'node:path';
-import { fileURLToPath } from 'node:url';
 import  homeRouter  from './routes/home.mjs';
 import  galleryRouter  from './routes/gallery.mjs';
 import  errorRouter  from './routes/error.mjs';
 
 
-const __fileName = fileURLToPath(import.meta.url); //URL del archivo actual
-const __dirName = dirname(__fileName); // saca el directorio a partir de la URL de filename
+const directorioName = path.dirname(new URL(import.meta.url).pathname); //direccion de la carpeta del directorio
 const app = express();
 const port = 3000;
 
-console.log(__dirName);
 
-//aca configuro la carpeta static
-//app.use(express.static(path.join(__dirName), 'public'));
+//aca configuro la carpeta public de los archivos estaticos
+app.use(express.static(path.join(directorioName, 'public')));
 
 //configuro pug aca
 app.set('view engine','pug'); //le indicamos a express que motor de renderizado vamnos a usar
-app.set('views', path.join(__dirname, 'views')); //le indicamos donde van a estar las vistas
+app.set('views', path.join(directorioName, 'views')); //le indicamos donde van a estar las vistas
 
 //llamo a la ruta de el home :)
 app.get('/', homeRouter);
