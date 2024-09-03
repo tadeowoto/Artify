057dd36e1a12a288fec6f87d0d3135ab52768855
tadeowoto <tadeo.woto@gmail.com>
2024-09-03 15:59:35 -0300

    pug layout and icons added

--- app.mjs
+++ app.mjs
@@ -4,8 +4,8 @@ import  homeRouter  from './routes/home.mjs';
 import  galleryRouter  from './routes/gallery.mjs';
 import  errorRouter  from './routes/error.mjs';
 
-
-const directorioName = path.dirname(new URL(import.meta.url).pathname); //direccion de la carpeta del directorio
+const directorioName = path.dirname(new URL(import.meta.url).pathname).substring(1);
+//const directorioName = path.dirname(new URL(import.meta.url).pathname); //direccion de la carpeta del directorio
 const app = express();
 const port = 3000;
 
@@ -15,7 +15,8 @@ app.use(express.static(path.join(directorioName, 'public')));
 
 //configuro pug aca
 app.set('view engine','pug'); //le indicamos a express que motor de renderizado vamnos a usar
-app.set('views', path.join(directorioName, 'views')); //le indicamos donde van a estar las vistas
+app.set('views', path.join(directorioName, 'views'));//le indicamos donde van a estar las vistas
+app.locals.basedir = path.join(directorioName, 'views'); 
 
 //llamo a la ruta de el home :)
 app.get('/', homeRouter);
