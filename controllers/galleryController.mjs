9b5ef1955dcdab1124adeac9d341a1607d51abc0
tadeowoto <tadeo.woto@gmail.com>
2024-09-06 09:58:11 -0300

    department filter added :)

--- controllers/galleryController.mjs
+++ controllers/galleryController.mjs
@@ -5,14 +5,20 @@ export class Gallery {
         { departmentId: 1, displayName: "American Decorative Arts" },
         { departmentId: 3, displayName: "Ancient Near Eastern Art" },
         { departmentId: 4, displayName: "Arms and Armor" },
-        { departmentId: 5, displayName: "Arts of Africa, Oceania, and the Americas" },
+        {
+          departmentId: 5,
+          displayName: "Arts of Africa, Oceania, and the Americas",
+        },
         { departmentId: 6, displayName: "Asian Art" },
         { departmentId: 7, displayName: "The Cloisters" },
         { departmentId: 8, displayName: "The Costume Institute" },
         { departmentId: 9, displayName: "Drawings and Prints" },
         { departmentId: 10, displayName: "Egyptian Art" },
         { departmentId: 11, displayName: "European Paintings" },
-        { departmentId: 12, displayName: "European Sculpture and Decorative Arts" },
+        {
+          departmentId: 12,
+          displayName: "European Sculpture and Decorative Arts",
+        },
         { departmentId: 13, displayName: "Greek and Roman Art" },
         { departmentId: 14, displayName: "Islamic Art" },
         { departmentId: 15, displayName: "The Robert Lehman Collection" },
@@ -31,41 +37,47 @@ export class Gallery {
         { locationId: 5, locationName: "Spain" },
       ];
 
-      const { department, keyword, location } = req.query;
+      
+      const  { department, keyword, location } = req.query;
+      if(department != undefined || keyword != undefined || location != undefined){
 
-      // Construir la URL de consulta con parámetros de filtro
-      let url = 'https://collectionapi.metmuseum.org/public/collection/v1/search?hasImages=true&q=""';
+        if(department == undefined){
+          department = 0;
+        }
+        if(keyword == undefined){
+          keyword = '';
+        }
+        if(location == undefined){
+          location = '';
+        }
+
+        const idsFiltrados = await getIdsFiltered(department,keyword,location);
+        const objetos = await getObjectDetails(idsFiltrados);
 
 
-      if(department && keyword && location){
-        if (department) {
-          url += `&departmentId=${department}`;
-        }
-  
-        if (keyword) {
-          url += `&q=${encodeURIComponent(keyword)}`;
-        }
-  
-        if (location) {
-          url += `&geoLocation=${encodeURIComponent(location)}`;
-        }
+        res.render("gallery/gallery", {
+          departments,
+          locations,
+          cards: objetos,
+        });
+
+      }else{
+        // Construir la URL de consulta con parámetros de filtro
+        let url ='https://collectionapi.metmuseum.org/public/collection/v1/search?hasImages=true&q=""';
+        const response = await fetch(url);
+        const data = await response.json(); //data tiene todos los ids de la url
+        const ids = data.objectIDs.slice(0, 100);// Limitar a 100 IDs para no sobrecargar la API
+        ids.sort(); 
+        const objetos = await getObjectDetails(ids);
+        res.render("gallery/gallery", {
+          departments,
+          locations,
+          cards: objetos,
+        });
       }
-
       
 
-      const response = await fetch(url);
-      const data = await response.json(); //data tiene todos los ids de la url
-      const ids = data.objectIDs.sort(); // Limitar a 100 IDs para no sobrecargar la API 
-      const objetos = await getObjectDetails(ids);
-
-
-
-      res.render("gallery/gallery", {
-        departments,
-        locations,
-        cards: objetos,
-      });
-
+      
     } catch (error) {
       console.error("Error al obtener los datos de la galería:", error);
       res.status(500).send("Hubo un error al cargar la galería.");
@@ -73,8 +85,26 @@ export class Gallery {
   }
 }
 
+async function getIdsFiltered(d,k,l){
+
+  let url = 'https://collectionapi.metmuseum.org/public/collection/v1/search?q=""&'
+  // /gallery?department=6&keyword=&location=
+  if(d != 0){
+    url = url+'departmentId='+d;
+  }
+
+  const response = await fetch(url);
+  const data = await response.json();
+  const ids = data.objectIDs.slice(0, 100);
+  ids.sort();
+  return ids;
+}
+
+
 async function getObjectDetails(objectIDs) {
   const objectDetails = [];
+
+  
 
   for (let i = 0; i < 20; i++) {
     const objectID = objectIDs[i];
@@ -82,10 +112,8 @@ async function getObjectDetails(objectIDs) {
       `https://collectionapi.metmuseum.org/public/collection/v1/objects/${objectID}`
     );
     const data = await response.json();
-    
-      objectDetails.push(data);
-    
+
+    objectDetails.push(data);
   }
   return objectDetails;
 }
-
